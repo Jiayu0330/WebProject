596863ba3840daf0a1ec53d44af0a75d22718603
Jiayu Jiang <you@example.com>
2019-04-23 21:42:51 -0400

    h

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
 var geoP = d3.json("data/world-countries.json")
-//var stateP = d3.csv("data/states.csv")
+var countriesP = d3.csv("data/Percentage_People_Using_Internet.csv")
 
 
-// Promise.all([geoP, stateP])
-//        .then(function(values)
-//        {
-//          var geoData = values[0];
-//          var stateData = values[1];
-//
-//          console.log(geoData, stateData);
-//
-//          var stateDict = {}
-//          stateData.forEach(function(state)
-//          {
-//            stateDict[state.NAME.trim()] = state; //trim() gets rid of spaces
-//          })
-//
-//          geoData.features.forEach(function(state)
-//          {
-//            state.properties.ABBR = stateDict[state.properties.name].ABBR;
-//          })
-//
-//          console.log(stateDict);
-//
-//          drawMap(geoData);
-//        })
+Promise.all([geoP, countriesP])
+       .then(function(values)
+       {
+         var geoData = values[0];
+         var countries = values[1];
+
+         console.log(geoData, countries);
+
+         var countriesDict = {}
+         countries.forEach(function(country)
+         {
+           countriesDict[country.CountryName] = country; //trim() gets rid of spaces
+         })
+
+         geoData.features.forEach(function(feature)
+         {
+           feature.properties.peopleUsingInternet = countriesDict[feature.properties.name_sort];
+         })
+
+         console.log(countriesDict);
+
+         drawMap(geoData);
+       })
 
 
-geoP.then(function(geoData)
-{
-  console.log(geoData);
-  drawMap(geoData);
-},
-function(err) {
-  console.log(err);
-})
+// geoP.then(function(geoData)
+// {
+//   console.log(geoData);
+//   drawMap(geoData);
+// },
+// function(err) {
+//   console.log(err);
+// })
 
 
 var drawMap = function(geoData)
 {
-  var screen = {width: 700, height: 600}
+  var screen = {width: 1200, height: 650}
 
-  var projection = d3.geoAlbersUsa()
+  var projection = d3.geoEqualEarth()
                      .translate([screen.width/2, screen.height/2])
-                     .scale([screen.width]);
+                     .scale([220]);
 
   var countryGenerator = d3.geoPath()
                          .projection(projection);
@@ -55,22 +55,177 @@ var drawMap = function(geoData)
               .attr("width", screen.width)
               .attr("height", screen.height);
 
+  // svg.selectAll("path")
+  //    .data(geoData.features)
+  //    .enter()
+  //    .append("path")
+  //    .attr("d", countryGenerator);
+
   var countries = svg.append("g")
                   .attr("id", "countries")
                   .selectAll("g")
                   .data(geoData.features) //array
+                  .enter()
                   .append("g")
-                  .classed("country", true);
+                  .classed("country", true)
 
   countries.append("path")
-        .attr("d", function(d) {
-          return stateGenerator(d);})
-        .attr("stroke", "red")
-        .attr("fill", "none");
+           .attr("d", countryGenerator)
+           .attr("id", function(d) {return d.properties.name;})
+           .attr("stroke", "black")
+           .attr("fill", "orange");
+
+  // d3.select("#United States")
+  //   .attr("fill", "red");
 
   countries.append("text")
-        .text(function(d) {return d.properties.ABBR;})
-        .attr("x", function(d) {return stateGenerator.centroid(d)[0];})
-        .attr("y", function(d) {return stateGenerator.centroid(d)[1];})
+           .attr("id", "countryAbbrev")
+           .text(function(d) {return d.properties.abbrev;})
+           .attr("x", function(d) {return countryGenerator.centroid(d)[0];})
+           .attr("y", function(d) {return countryGenerator.centroid(d)[1];})
+           .attr("font-size", "14px")
 
+  //Panning the map
+  var panning = 30;
+  //up
+  var north = svg.append("g")
+                 .attr("class", "pan")
+                 .attr("id", "north");
+
+  north.append("rect")
+       .attr("x", 0)
+       .attr("y", 0)
+       .attr("width", screen.width)
+       .attr("height", panning)
+       .attr("fill", "grey")
+       .attr("opacity", 0.4)
+
+  north.append("text")
+       .attr("x", screen.width/2)
+       .attr("y", panning - 10)
+       .html("&uarr;") //arrow
+
+  //down
+  var south = svg.append("g")
+                .attr("class", "pan")
+                .attr("id", "south");
+
+  south.append("rect")
+      .attr("x", 0)
+      .attr("y", screen.height - panning)
+      .attr("width", screen.width)
+      .attr("height", panning)
+      .attr("fill", "grey")
+      .attr("opacity", 0.4)
+
+  south.append("text")
+      .attr("x", screen.width/2)
+      .attr("y", screen.height - 10)
+      .html("&darr;") //arrow
+
+  //west
+  var west = svg.append("g")
+                .attr("class", "pan")
+                .attr("id", "west");
+
+  west.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", panning)
+      .attr("height", screen.height)
+      .attr("fill", "grey")
+      .attr("opacity", 0.4)
+
+  west.append("text")
+      .attr("x", 3)
+      .attr("y", screen.height/2)
+      .html("&larr;") //arrow
+
+  //east
+  var east = svg.append("g")
+                .attr("class", "pan")
+                .attr("id", "east");
+
+  east.append("rect")
+      .attr("x", screen.width - panning)
+      .attr("y", 0)
+      .attr("width", panning)
+      .attr("height", screen.height)
+      .attr("fill", "grey")
+      .attr("opacity", 0.4)
+
+  east.append("text")
+      .attr("x", screen.width - 27)
+      .attr("y", screen.height/2)
+      .html("&rarr;") //arrow
+
+  d3.selectAll(".pan")
+    .on("click", function()
+    {
+      var offset = projection.translate(); //get current translation offset
+      var moveAmount = 50; //how much to move on each click
+      var direction = d3.select(this).attr("id"); //which Norway
+
+      switch(direction)
+      {
+        case "north":
+            offset[1] += moveAmount;
+            break;
+        case "south":
+            offset[1] -= moveAmount;
+            break;
+        case "west":
+            offset[0] += moveAmount;
+            break;
+        case "east":
+            offset[0] -= moveAmount;
+            break;
+        default:
+            break;
+      }
+
+      projection.translate(offset);
+
+      svg.selectAll("path")
+         .transition()
+         .attr("d", countryGenerator);
+
+      svg.selectAll("#countryAbbrev")
+         .transition()
+         .attr("x", function(d) {return countryGenerator.centroid(d)[0];})
+         .attr("y", function(d) {return countryGenerator.centroid(d)[1];})
+    })
+
+  //dragging the map
+  var dragging = function(d) {
+    var offset = projection.translate();
+
+    offset[0] += d3.event.dx;
+    offset[1] += d3.event.dy;
+
+    projection.translate(offset);
+
+    map.selectAll("path")
+       .transition()
+       .attr("d", countryGenerator);
+
+    map.selectAll("#countryAbbrev")
+       .transition()
+       .attr("x", function(d) {return countryGenerator.centroid(d)[0];})
+       .attr("y", function(d) {return countryGenerator.centroid(d)[1];})
+  }
+
+  var drag = d3.drag()
+               .on("drag", dragging);
+
+  var map = svg.append("g")
+               .attr("id", "map")
+               .call(drag);
+
+  map.append("rect")
+     .attr("x", 0)
+     .attr("y", 0)
+     .attr("width", screen.width)
+     .attr("height", screen.height)
+     .attr("opacity", 0);
 }
