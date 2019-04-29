7e99412a57447919a60680ef9fc74d37bf448e8d
Jiayu Jiang <you@example.com>
2019-04-29 15:23:59 -0400

    h

--- script.js
+++ script.js
@@ -77,8 +77,7 @@ var drawMap = function(geoData)
            .on("mouseover", function(d, i) {
              // var path = d3.select(this)._groups[0][0];
              // console.log(path);
-             svg.append("g")
-                .append("text")
+             svg.append("text")
                 .attr("id", "text" + i)
                 .attr("x", countryGenerator.centroid(d)[0] - countryGenerator.centroid(d)[0] * 0.02)
                 .attr("y", countryGenerator.centroid(d)[1] + countryGenerator.centroid(d)[1] * 0.02)
@@ -98,7 +97,7 @@ var drawMap = function(geoData)
   //year label
   svg.append("text")
      .attr("id", "year_label")
-     .attr("x", screen.width/2 - 65)
+     .attr("x", screen.width/2 - 68)
      .attr("y", screen.height - 70)
      .text("Before 1990")
 
@@ -107,16 +106,30 @@ var drawMap = function(geoData)
   });
   //console.log("mapped array:", mapped);
 
-  d3.select("#CHN")
+  var timesRun = 0;
+
+  d3.select("#play-button")
     .on("click", function() {
-      var timesRun = 0;
-      var interval = setInterval(function() {
-        animation(mapped, timesRun);
-        timesRun += 1;
-        if (timesRun == 27) {
-          clearInterval (interval);
-        }
-      }, 500);
+      var button = d3.select(this);
+      var year = d3.select("#year_label");
+      if (year.text() == "2016") {
+        timesRun = 0;
+      }
+      if (button.text() == "Pause") {
+        clearInterval(timer);
+        button.text("Play");
+      }
+      else {
+        timer = setInterval(function() {
+          animation(mapped, timesRun);
+          timesRun += 1;
+          if (timesRun == 27) {
+            clearInterval (timer);
+            button.text("Replay");
+          }
+        }, 500);
+        button.text("Pause");
+      }
     })
 
   var animation = function(mapped, timesRun) {
@@ -133,7 +146,7 @@ var drawMap = function(geoData)
 
     d3.select("#year_label")
       .text(currentYear_i)
-      .attr("x", 575);
+      .attr("x", 573);
 
     for (var i = 0; i < 175; i++) {
       //console.log(mapped[i]);
@@ -171,17 +184,48 @@ var drawMap = function(geoData)
                  .attr("id", "north");
 
   north.append("rect")
+       .attr("id", "north_rect")
        .attr("x", 0)
        .attr("y", 0)
        .attr("width", screen.width)
        .attr("height", panning)
-       .attr("fill", "grey")
-       .attr("opacity", 0.4);
+       .attr("fill", "#931621")
+       .attr("opacity", 0.2)
+       .on("mouseover", function() {
+         var arrow = d3.select("#north_arrow")
+         arrow.attr("width", 40)
+              .attr("x", screen.width/2 - 20)
+              .attr("y", panning - 35)
+       })
+       .on("mouseout", function() {
+         var arrow = d3.select("#north_arrow")
+         arrow.attr("width", 32)
+              .attr("x", screen.width/2 - 16)
+              .attr("y", panning - 30)
+       });
 
-  north.append("text")
-       .attr("x", screen.width/2)
-       .attr("y", panning - 10)
-       .html("&uarr;"); //arrow
+  north.append("svg:image")
+       .attr("xlink:href", "icons/up.png")
+       .attr("id", "north_arrow")
+       .attr("x", screen.width/2 - 16)
+       .attr("y", panning - 30)
+       .attr("width", 32)
+       .on("mouseover", function() {
+         var rect = d3.select("#north_rect")
+         rect.attr("opacity", "0.4");
+         var arrow = d3.select("#north_arrow")
+         arrow.attr("width", 40)
+              .attr("x", screen.width/2 - 20)
+              .attr("y", panning - 35)
+       })
+       .on("mouseout", function() {
+         var rect = d3.select("#north_rect")
+         rect.attr("opacity", "0.2");
+         var arrow = d3.select("#north_arrow")
+         arrow.attr("width", 32)
+              .attr("x", screen.width/2 - 16)
+              .attr("y", panning - 30)
+       });
 
   //down
   var south = svg.append("g")
@@ -189,17 +233,48 @@ var drawMap = function(geoData)
                 .attr("id", "south");
 
   south.append("rect")
+      .attr("id", "south_rect")
       .attr("x", 0)
       .attr("y", screen.height - panning)
       .attr("width", screen.width)
       .attr("height", panning)
-      .attr("fill", "grey")
-      .attr("opacity", 0.4);
+      .attr("fill", "#931621")
+      .attr("opacity", 0.2)
+      .on("mouseover", function() {
+        var arrow = d3.select("#south_arrow")
+        arrow.attr("width", 40)
+             .attr("x", screen.width/2 - 20)
+             .attr("y", screen.height - 35)
+      })
+      .on("mouseout", function() {
+        var arrow = d3.select("#south_arrow")
+        arrow.attr("width", 32)
+             .attr("x", screen.width/2 - 16)
+             .attr("y", screen.height - 30)
+      });
 
-  south.append("text")
-      .attr("x", screen.width/2)
-      .attr("y", screen.height - 10)
-      .html("&darr;"); //arrow
+  south.append("svg:image")
+       .attr("xlink:href", "icons/down.png")
+       .attr("id", "south_arrow")
+       .attr("x", screen.width/2 - 16)
+       .attr("y", screen.height - 30)
+       .attr("width", "32")
+       .on("mouseover", function() {
+         var rect = d3.select("#south_rect")
+         rect.attr("opacity", "0.4");
+         var arrow = d3.select("#south_arrow")
+         arrow.attr("width", 40)
+              .attr("x", screen.width/2 - 20)
+              .attr("y", screen.height - 35)
+       })
+       .on("mouseout", function() {
+         var rect = d3.select("#south_rect")
+         rect.attr("opacity", "0.2");
+         var arrow = d3.select("#south_arrow")
+         arrow.attr("width", 32)
+              .attr("x", screen.width/2 - 16)
+              .attr("y", screen.height - 30)
+       });
 
   //west
   var west = svg.append("g")
@@ -207,17 +282,48 @@ var drawMap = function(geoData)
                 .attr("id", "west");
 
   west.append("rect")
+      .attr("id", "west_rect")
       .attr("x", 0)
       .attr("y", 0)
       .attr("width", panning)
       .attr("height", screen.height)
-      .attr("fill", "grey")
-      .attr("opacity", 0.4);
+      .attr("fill", "#931621")
+      .attr("opacity", 0.2)
+      .on("mouseover", function() {
+        var arrow = d3.select("#west_arrow")
+        arrow.attr("width", 40)
+             .attr("x", -6)
+             .attr("y", screen.height/2 - 36)
+      })
+      .on("mouseout", function() {
+        var arrow = d3.select("#west_arrow")
+        arrow.attr("width", 32)
+             .attr("x", -2)
+             .attr("y", screen.height/2 - 32)
+      });
 
-  west.append("text")
-      .attr("x", 15)
-      .attr("y", screen.height/2)
-      .html("&larr;"); //arrow
+  west.append("svg:image")
+       .attr("xlink:href", "icons/left.png")
+       .attr("id", "west_arrow")
+       .attr("x", -2)
+       .attr("y", 293)
+       .attr("width", "32")
+       .on("mouseover", function() {
+         var rect = d3.select("#west_rect")
+         rect.attr("opacity", "0.4");
+         var arrow = d3.select("#west_arrow")
+         arrow.attr("width", 40)
+              .attr("x", -6)
+              .attr("y", screen.height/2 - 36)
+       })
+       .on("mouseout", function() {
+         var rect = d3.select("#west_rect")
+         rect.attr("opacity", "0.2");
+         var arrow = d3.select("#west_arrow")
+         arrow.attr("width", 32)
+              .attr("x", -2)
+              .attr("y", screen.height/2 - 32)
+       });
 
   //east
   var east = svg.append("g")
@@ -225,17 +331,48 @@ var drawMap = function(geoData)
                 .attr("id", "east");
 
   east.append("rect")
+      .attr("id", "east_rect")
       .attr("x", screen.width - panning)
       .attr("y", 0)
       .attr("width", panning)
       .attr("height", screen.height)
-      .attr("fill", "grey")
-      .attr("opacity", 0.4);
+      .attr("fill", "#931621")
+      .attr("opacity", 0.2)
+      .on("mouseover", function() {
+        var arrow = d3.select("#east_arrow")
+        arrow.attr("width", 40)
+             .attr("x", screen.width - 33)
+             .attr("y", screen.height/2 - 36)
+      })
+      .on("mouseout", function() {
+        var arrow = d3.select("#east_arrow")
+        arrow.attr("width", 32)
+             .attr("x", screen.width - 29)
+             .attr("y", screen.height/2 - 32)
+      });
 
-  east.append("text")
-      .attr("x", screen.width - 15)
-      .attr("y", screen.height/2)
-      .html("&rarr;"); //arrow
+  east.append("svg:image")
+       .attr("xlink:href", "icons/right.png")
+       .attr("id", "east_arrow")
+       .attr("x", screen.width - 29)
+       .attr("y", screen.height/2 - 32)
+       .attr("width", 32)
+       .on("mouseover", function() {
+         var rect = d3.select("#east_rect")
+         rect.attr("opacity", "0.4");
+         var arrow = d3.select("#east_arrow")
+         arrow.attr("width", 40)
+              .attr("x", screen.width - 33)
+              .attr("y", screen.height/2 - 36)
+       })
+       .on("mouseout", function() {
+         var rect = d3.select("#east_rect")
+         rect.attr("opacity", "0.2");
+         var arrow = d3.select("#east_arrow")
+         arrow.attr("width", 32)
+              .attr("x", screen.width - 29)
+              .attr("y", screen.height/2 - 32)
+       });
 
   d3.selectAll(".pan")
     .on("click", function()
@@ -407,6 +544,5 @@ var drawMap = function(geoData)
       map.transition()
     		 .call(zoom.scaleBy, scaleFactor);
     })
-
 
 }
